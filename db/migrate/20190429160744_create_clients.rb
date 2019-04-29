e5b7ac839ef63ea85c8cae0d619a836dffd342d5
Richard <orsusbass@gmail.com>
2019-04-29 16:16:53 +0000

    create client model

---
+++ db/migrate/20190429160744_create_clients.rb
@@ -0,0 +1,18 @@
+class CreateClients < ActiveRecord::Migration[5.2]
+  def change
+    create_table :clients do |t|
+      t.string :name
+      t.string :street
+      t.string :city
+      t.string :state
+      t.string :zipcode
+      t.integer :status
+      t.datetime :onboard_date
+      t.datetime :offboard_date
+      t.datetime :recent_request_date
+      t.boolean :account_current default: true
+
+      t.timestamps
+    end
+  end
+end
