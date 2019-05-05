c6ac98e53abb1c7ec971d22c948996e9e70cb5b4
Richard <orsusbass@gmail.com>
2019-05-05 01:28:15 +0000

    client page sling

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
     namespace :admin do
+      get 'dashboard', to: 'pages#dashboard'
       resources :clients
     end
   
