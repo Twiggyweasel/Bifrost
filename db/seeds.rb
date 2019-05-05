e3804c86d2c016986218b1e44683689d41921fb2
Richard <orsusbass@gmail.com>
2019-05-05 00:24:25 +0000

    install fontawesome

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Client.create(name: "Company 1", street: '123 Main St', city: 'Mega City', state: 'KS', zipcode: '66215', status: 0, service_type: 0, payment_method: 0)
+Client.create(name: "Company 2", street: '123 Main St', city: 'Mega City', state: 'KS', zipcode: '66215', status: 0, service_type: 0, payment_method: 0)
 
 4.times do 
     Ticket.create(request_summary: "Big Request", request_detail: "I already told you this is a big request", severity: 3, status: 0, client: Client.first)
+end
+
+4.times do 
+    Ticket.create(request_summary: "Big Request", request_detail: "I already told you this is a big request", severity: 3, status: 0, client: Client.last)
 end
